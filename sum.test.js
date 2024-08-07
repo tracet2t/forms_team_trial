b0371d2ac21bcddbebe3bddda24efa3afc5625eb
Aynul Amna <amnabinthaman@gmail.com>
2024-08-07 17:39:18 +0530

    Jest Testing Samples

---
+++ sum.test.js
@@ -0,0 +1,22 @@
+const sum = require('./sum');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+}
+);
+
+test('adds 0 + 0 to equal 0', () => {
+    expect(sum(0, 0)).toBe(0);
+  });
+  
+  test('adds -1 + 1 to equal 0', () => {
+    expect(sum(-1, 1)).toBe(0);
+  });
+  
+  test('adds 100 + 200 to equal 300', () => {
+    expect(sum(100, 200)).toBe(300);
+  });
+  
+  test('adds -100 + -200 to equal -300', () => {
+    expect(sum(-100, -200)).toBe(-300);
+  });
