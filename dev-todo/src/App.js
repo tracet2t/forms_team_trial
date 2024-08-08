b78c91d06b6299f073e9d3b7f00a15b70d05e2d6
P.K.U.Devaki <s92062603@ousl.lk>
2024-08-08 17:42:45 +0530

    Add todo input

--- dev-todo/src/App.js
+++ dev-todo/src/App.js
@@ -5,6 +5,43 @@ function App() {
   return (
     <div className="App">
       <h1> FIND FOOD </h1>
+      <div className= "todo-wrap">
+        <div className="input">
+          
+       
+            <div className='input-item'>    
+                    <label> Title </label>
+            <input type="text" placeholder='Add your food type here'></input>
+            </div>
+
+            <div className='input-item'>    
+                    <label> Title </label>
+            <input type="text" placeholder='Add your food type here'></input>
+            </div>
+
+            <div className='input-item'>    
+                    <label> Title </label>
+            <input type="text" placeholder='Add your food type here'></input>
+            </div>
+
+            <div className='input-item'>    
+                    <label> Title </label>
+            <input type="text" placeholder='Add your food type here'></input>
+            </div>
+
+            <div className='input-item'>    
+                    <label> Title </label>
+            <input type="text" placeholder='Add your food type here'></input>
+            </div>
+            
+            <div className='input-item'>    
+                    <label> Title </label>
+            <input type="text" placeholder='Add your food type here'></input>
+            </div>
+          
+        </div>
+      </div>
+
      
     </div>
   );
