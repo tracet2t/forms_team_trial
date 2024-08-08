462a92832260c7ec8468ae4426b40e3bf866a48a
P.K.U.Devaki <s92062603@ousl.lk>
2024-08-08 09:38:45 +0530

    Resolved merge conflicts

--- sum.js
+++ sum.js
@@ -1,10 +1,6 @@
 <<<<<<< HEAD
 function sum(a, b) {
     return a + b;
-  }
-=======
-function sum(a, b) {
-    return a + b;
-  }
+
 >>>>>>> 018220b613b91b57ad8dccba0205e758ab8ee98b
   module.exports = sum;
