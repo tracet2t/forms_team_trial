739e834db5d53e30f8f6827bfb425b443099c4fb
P.K.U.Devaki <s92062603@ousl.lk>
2024-08-08 18:43:34 +0530

    test

--- dev-todo/src/App.js
+++ dev-todo/src/App.js
@@ -1,57 +1,48 @@
-
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1> FIND FOOD </h1>
-      <div className= "todo-wrap">
+      <div className="todo-wrap">
         <div className="input">
-          
-       
-            <div className='input-item'>    
-                    <label> Title </label>
-            <input type="text" placeholder='Add your food type here'></input>
-            </div>
-<br>
-</br>
-            <div className='input-item'>    
-                    <label> Description</label>
-            <input type="text" placeholder='Add your food description here'></input>
-            </div>
-            <br>
-            </br>
-            <div className='input-item'>    
-              <button type='button' className ="Button1"> Add </button>
-                </div>    
-                <br>
-                </br>
-            <div className='input-item'>    
-                    <label> Title </label>
-            <input type="text" placeholder='Add your food type here'></input>
-            </div>
-            <br>
-            </br>
-            <div className='input-item'>    
-                    <label> Title </label>
-            <input type="text" placeholder='Add your food type here'></input>
-            </div>
-            <br>
-            </br>
-            <div className='input-item'>    
-                    <label> Title </label>
-            <input type="text" placeholder='Add your food type here'></input>
-            </div>
-            <br>
-            </br>
+          <div className="input-item">
+            <label> Title </label>
+            <input type="text" placeholder="Add your food type here" />
+          </div>
+          <br />
+          <div className="input-item">
+            <label> Description</label>
+            <input type="text" placeholder="Add your food description here" />
+          </div>
+          <br />
+          <div className="input-item">
+            <button type="button" className="Button1">
+              Add
+            </button>
+          </div>
+          <br />
+          <div className="input-item">
+            <label> Title </label>
+            <input type="text" placeholder="Add your food type here" />
+          </div>
+          <br />
+          <div className="input-item">
+            <label> Title </label>
+            <input type="text" placeholder="Add your food type here" />
+          </div>
+          <br />
+          <div className="input-item">
+            <label> Title </label>
+            <input type="text" placeholder="Add your food type here" />
+          </div>
+          <br />
         </div>
         <div className="btn-area">
-          <button> New  </button>
-          <button> New  </button>
+          <button> New </button>
+          <button> New </button>
         </div>
       </div>
-
-     
     </div>
   );
 }
