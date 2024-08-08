e6545fc8251459225ec34327ff6567cd03dc65c7
P.K.U.Devaki <s92062603@ousl.lk>
2024-08-08 17:58:25 +0530

    add button

--- dev-todo/src/App.js
+++ dev-todo/src/App.js
@@ -13,32 +13,41 @@ function App() {
                     <label> Title </label>
             <input type="text" placeholder='Add your food type here'></input>
             </div>
-
+<br>
+</br>
+            <div className='input-item'>    
+                    <label> Description</label>
+            <input type="text" placeholder='Add your food description here'></input>
+            </div>
+            <br>
+            </br>
+            <div className='input-item'>    
+              <button type='button' className ="Button1"> Add </button>
+                </div>    
+                <br>
+                </br>
             <div className='input-item'>    
                     <label> Title </label>
             <input type="text" placeholder='Add your food type here'></input>
             </div>
-
+            <br>
+            </br>
             <div className='input-item'>    
                     <label> Title </label>
             <input type="text" placeholder='Add your food type here'></input>
             </div>
-
+            <br>
+            </br>
             <div className='input-item'>    
                     <label> Title </label>
             <input type="text" placeholder='Add your food type here'></input>
             </div>
-
-            <div className='input-item'>    
-                    <label> Title </label>
-            <input type="text" placeholder='Add your food type here'></input>
-            </div>
-            
-            <div className='input-item'>    
-                    <label> Title </label>
-            <input type="text" placeholder='Add your food type here'></input>
-            </div>
-          
+            <br>
+            </br>
+        </div>
+        <div className="btn-area">
+          <button> New  </button>
+          <button> New  </button>
         </div>
       </div>
 
