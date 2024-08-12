bf721c2a949860d6110a6af1c897a512a30ef12d
P.K.U.Devaki <s92062603@ousl.lk>
2024-08-12 17:40:22 +0530

    export add todo function

--- dev-todo/backend/controllers/todoController.js
+++ dev-todo/backend/controllers/todoController.js
@@ -7,3 +7,19 @@ exports.getAllTodos = (req, res) => {
     res.json(results);
   });
 };
+exports.getCompletedTodos = (req, res) => {
+    const sql = 'SELECT * FROM todos WHERE completed = 1';
+    connection.query(sql, (err, results) => {
+      if (err) throw err;
+      res.json(results);
+    });
+  };
+  exports.addTodo = (req, res) => {
+    const { title, description, dueDate } = req.body;
+    const sql = 'INSERT INTO todos (title, description, dueDate, completed) VALUES (?, ?, ?, 0)';
+    connection.query(sql, [title, description, dueDate], (err, result) => {
+      if (err) throw err;
+      res.json({ id: result.insertId, title, description, dueDate, completed: 0 });
+    });
+  };
+  
