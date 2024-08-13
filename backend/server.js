8f7e26ffaead8213a4204c416a22699e84c5bb8c
P.K.U.Devaki <s92062603@ousl.lk>
2024-08-13 15:05:37 +0530

    New changes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,10 @@
 const express = require ("express");
 const app  = express();
+const cors = require ("cors");
+const mysql = require ("mysql");
+
+
+app.use(cors());
 
 app.listen( 8081,()  =>  {
     console.log ("Listening");
