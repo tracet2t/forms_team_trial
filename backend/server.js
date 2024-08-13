4c4a7f5a32c6bdc5f85ee687538e38c86f40c980
P.K.U.Devaki <s92062603@ousl.lk>
2024-08-13 15:59:09 +0530

    Add directory

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,23 @@
-const express = require ("express");
-const app  = express();
-const cors = require ("cors");
-const mysql = require ("mysql");
-
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mysql = require("mysql");
 
 app.use(cors());
 
-app.listen( 8081,()  =>  {
-    console.log ("Listening");
+mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Kani_0203",
+    database: "todolist_db"
+})
 
+app.get("/", (req,res) => {
+    res.json("hey backend");
 
 })
+
+const PORT = 8082;
+app.listen(8082, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
