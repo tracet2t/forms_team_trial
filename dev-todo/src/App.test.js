dd3d4605ddc43bf868a10d0d172e255e78b92c89
P.K.U.Devaki <s92062603@ousl.lk>
2024-08-13 13:29:07 +0530

    start

--- dev-todo/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';  // Correctly import the App component
-
-test('renders FIND FOOD title', () => {
-  render(<App />);
-  const titleElement = screen.getByText(/FIND FOOD/i);
-  expect(titleElement).toBeInTheDocument();
-});
