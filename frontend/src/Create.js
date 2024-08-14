2a8516d32531fc33c1a3d8b0bd6da6b7d43eb271
P.K.U.Devaki <s92062603@ousl.lk>
2024-08-14 18:15:03 +0530

    add email

--- frontend/src/Create.js
+++ frontend/src/Create.js
@@ -2,24 +2,32 @@ import React from 'react'
 
 function Create() {
   return (
-    <div>
+    
          <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
             <div className='w-75 bg-white rounded p-3'>
                 <form>
                     <h2>Add member </h2>
-                <div className = 'mb-20'>
-                    
-
+                <div className = 'mb-2'>
+                    <label htmlFor="">Name</label>
+                    <input type ="text" placeholder='enter name' className='form'/>
                 </div>
+                <div className = 'mb-2'>
+                <label htmlFor="">E-mail</label>
+                    <input type ="text" placeholder='enter e-mail' className='form'/>
+                </div>
+                <button className = 'btn btn-success'>Submit</button>
+
+                
     
 
                 </form>
               
               
                 </div>
-        </div>
+        
     </div>
   )
 }
+
 
 export default Create;
