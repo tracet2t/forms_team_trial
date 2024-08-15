2d8c9364e28b6d2cf4fda91dc465a61ae50546cc
P.K.U.Devaki <s92062603@ousl.lk>
2024-08-15 15:56:21 +0530

    prepare connection

--- frontend/src/Create.js
+++ frontend/src/Create.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 import './Create.css'; 
 
 function Create() {
+    const[name, setName] = useState('')
+    const[email, setEmail] = useState('')
+    function handleSubmit (event){
+        event.preventDefault();
+        axios.post('http://localhost:8085/create', {name, email})
+    }
     return (
         <div className='container'>
             <div className='form-container'>
-                <form>
+                <form onSubmit = {handleSubmit}> 
                     <h2>Add Member</h2>
                     <div className='form-group'>
                         <label htmlFor="name">Name</label>
-                        <input type="text" id="name" placeholder='Enter name' />
+                        <input type="text" id="name" placeholder='Enter name'
+                         onChange ={e => setName(e.target.value)}
+                          />
+                       
+                        
                     </div>
                     <div className='form-group'>
                         <label htmlFor="email">E-mail</label>
-                        <input type="text" id="email" placeholder='Enter e-mail' />
+                        <input type="text" id="email" placeholder='Enter e-mail'
+                        onChange ={e => setEmail(e.target.value)}
+                         />
                     </div>
                     <button className='btn-submit'>Submit</button>
                 </form>
